b2c1a13022714b23f883edfd1afd9c954656c3ea
Debdeep Banerjee <debdeepbanerjee@Debdeeps-MacBook-Air.local>
2025-03-11 11:28:32 +0530

    Converting to ReactJS for frontend

--- dev1/src/App.js
+++ dev1/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import About from './components/About'; // New React page
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Redirects to a static HTML page */}
+        <Route path="/" element={<StaticPage url="/home.html" />} />
+        <Route path="/login" element={<StaticPage url="/login.html" />} />
+        <Route path="/dashboard" element={<StaticPage url="/next_index.html" />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
   );
+}
+
+// Component to load static HTML pages
+function StaticPage({ url }) {
+  React.useEffect(() => {
+    window.location.href = url; // Redirect to static HTML file
+  }, [url]);
+  
+  return null; // No JSX needed
 }
 
 export default App;
